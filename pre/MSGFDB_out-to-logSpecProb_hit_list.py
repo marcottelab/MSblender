29d525bf7cd1bcd85833d9fe118161439a8529a3
Taejoon Kwon <taejoon.kwon@mail.utexas.edu>
2010-12-20 12:05:49 -0600

    - Support MSGFDB outuput. Add script to compose msblender input file (msblender_in).

---
+++ pre/MSGFDB_out-to-logSpecProb_hit_list.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import math
+import os
+import sys
+
+usage_mesg = 'Usage: MSGFDB_out-to-logSpecProb_hit_list.py <MS-GFDB out file>'
+
+if( len(sys.argv) != 2 ):
+    print usage_mesg
+    sys.exit(1)
+
+filename_in = sys.argv[1]
+if( not os.access(filename_in,os.R_OK) ):
+    print "%s is not accessible."%filename_in
+    print usage_mesg
+    sys.exit(1)
+
+filename_out = '%s.logSpecProb_hit_list'%filename_in
+sys.stderr.write("Write %s ... "%filename_out)
+f_out = open(filename_out,'w')
+f_out.write("#input : %s\n"%filename_in)
+f_out.write("#Spectrum_id\tCharge\tNeutralMass\tPeptide\tProtein\tMissedCleavages\tScore(-log10[SpecProb])\n")
+f_in = open(filename_in,'r')
+for line in f_in:
+    if( line.startswith('#') ):
+        continue
+    tokens = line.strip().split("\t")
+    filename = os.path.basename(tokens[0]).split('.')[0]
+    scan_id = int(tokens[1])
+
+    pep_seq = tokens[6].split('.')[1]
+    prot_id = tokens[7].replace('XXX.','xf_')
+    charge = int(tokens[5])
+    precursor_mz = float(tokens[3])
+    neutral_mass = precursor_mz * charge
+    SpecProb = float(tokens[-1]) 
+    sp_id = '%s.%05d.%05d.%d'%(filename,scan_id,scan_id,charge)
+    f_out.write("%s\t%d\t%f\t%s\t%s\t-1\t%f\n"%(sp_id,charge,neutral_mass,pep_seq,prot_id,-1.0*math.log10(SpecProb)))
+f_in.close()
+f_out.close()
+sys.stderr.write("Done\n")
---
+++ pre/make-msblender_in.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import os 
+import sys
+
+usage_mesg = 'Usage: make-msblender_in.py <msblender_in.conf file>'
+
+sp2hit = dict()
+search_engine_list = []
+f_conf = open(sys.argv[1],'r')
+for line in f_conf:
+    if( line.startswith('#') ):
+        continue
+    tokens = line.strip().split()
+    search_engine_name = tokens[0]
+    search_engine_list.append( search_engine_name )
+
+    filename_hit_list = tokens[1]
+    f_hit_list = open(filename_hit_list,'r')
+    for line in f_hit_list:
+        if( line.startswith('#') ):
+            continue
+        tokens = line.strip().split("\t")
+        sp_id =  tokens[0]
+        charge = tokens[1]
+        pep_seq = tokens[3]
+        prot_id = tokens[4]
+        sp_pep_id = '%s.%s'%(sp_id,pep_seq)
+        if( not sp2hit.has_key( sp_pep_id ) ):
+            sp2hit[sp_pep_id] = dict()
+        sp2hit[sp_pep_id][search_engine_name] = {'prot_id':prot_id, 'score': float(tokens[-1])}
+    f_hit_list.close()
+f_conf.close()
+
+search_engine_list = sorted(search_engine_list)
+
+print "sp_pep_id\tdecoy\t%s"%("\t".join(["%s_score"%x for x in search_engine_list]))
+for sp_pep_id in sorted(sp2hit.keys()):
+    output_list = []
+    is_decoy = 0
+    for tmp_search_engine_name in search_engine_list:
+        if( sp2hit[sp_pep_id].has_key( tmp_search_engine_name ) ):
+            output_list.append( "%f"%sp2hit[sp_pep_id][tmp_search_engine_name]['score'] )
+            if( sp2hit[sp_pep_id][tmp_search_engine_name]['prot_id'].startswith('xf_') ):
+                is_decoy = 1
+        else:
+            output_list.append( 'NA' )
+
+    print "%s\t%d\t%s"%(sp_pep_id, is_decoy, '\t'.join(output_list) )
