951bb791832f63d2b9eca880bd62dbe1ed74dbda
Taejoon Kwon <taejoon.kwon@mail.utexas.edu>
2011-01-30 13:08:47 -0600

    rename myrimatch_pepxml-to-mvh_hit_list.py for consistency

--- pre/myrimatch_pepxml-to-mvh_hit_list.py
+++ pre/myrimatch_pepxml-to-mvh_hit_list.py
@@ -4,7 +4,7 @@ import sys
 import re
 import pepxml
 
-usage_mesg = 'Usage: myrimatch-pepxml2hit_list.py <.pepxml file>'
+usage_mesg = 'Usage: myrimatch_pepxml-to-mvh_hit_list.py <.pepxml file>'
 
 if( len(sys.argv) != 2 ):
     print usage_mesg
@@ -20,19 +20,20 @@ PSM = pepxml.parse_by_filename(filename_pepxml)
 
 filename_out = filename_pepxml
 filename_out = re.sub('.pepxml$','',filename_out)
-filename_out += '.hit_list'
+filename_out += '.mvh_hit_list'
 sys.stderr.write("Write %s ... \n"%filename_out)
 f_out = open(filename_out,'w')
 f_out.write("# pepxml: %s\n"%filename_pepxml)
-f_out.write("#Spectrum_id\tCharge\tNeutralMass\tPeptide\tProtein\tMissedCleavages\tAbsScore(mvh)\tRelScore(mzMAE)\n")
+f_out.write("#Spectrum_id\tCharge\tPrecursorMz\tMassDiff\tPeptide\tProtein\tMissedCleavages\tScore(mvh)\n")
 for spectrum_id in PSM.keys():
     charge = PSM[spectrum_id]['charge']
-    neutral_mass = PSM[spectrum_id]['neutral_mass']
+    precursor_mz = PSM[spectrum_id]['precursor_mz']
     best_peptide = ''
     best_protein = ''
     best_mvh = -100
     best_mzMAE = 0
     missed_cleavages = 0
+    massdiff = 0
     for tmp_hit in PSM[spectrum_id]['search_hit']:
         if( tmp_hit['hit_rank'] != 1 ):
             continue
@@ -42,6 +43,7 @@ for spectrum_id in PSM.keys():
             best_protein = tmp_hit['protein']
             best_mzFidelity= tmp_hit['mzFidelity']
             missed_cleavages = tmp_hit['missed_cleavages']
+            massdiff = tmp_hit['massdiff']
     if( best_mvh > -100 ):
-        f_out.write("%s\t%s\t%f\t%s\t%s\t%d\t%f\t%f\n"%(spectrum_id,charge,neutral_mass,best_peptide,best_protein,missed_cleavages,best_mvh,best_mzFidelity))
+        f_out.write("%s\t%s\t%f\t%f\t%s\t%s\t%d\t%f\n"%(spectrum_id,charge,precursor_mz,massdiff,best_peptide,best_protein,missed_cleavages,best_mvh))
 f_out.close()
