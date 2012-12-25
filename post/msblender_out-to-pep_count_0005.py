7d1e32e33eb18f5ca48ffca2f85ac3726d1a3f22
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2012-12-25 16:01:28 -0600

    Report pep_count for each sample

--- post/msblender_out-to-pep_count_0005.py
+++ post/msblender_out-to-pep_count_0005.py
@@ -25,6 +25,7 @@ count_D = 0
 
 D_pep_count = dict()
 pep_count = dict()
+sample_list = []
 f_log = open('%s.pep_count_FDR0005.log'%filename_base,'w')
 f_log.write('PSM_id\tFDR\tmvScore\n')
 for tmp_psm in sorted(psm_mvScore.keys(),key=psm_mvScore.get,reverse=True):
@@ -43,18 +44,35 @@ for tmp_psm in sorted(psm_mvScore.keys(),key=psm_mvScore.get,reverse=True):
     if( tmp_FDR < FDR_cutoff ):
         if( psm_TD[tmp_psm] == 'F' ):
             f_log.write('%s\t%.3f\t%.2f\n'%(tmp_psm,tmp_FDR,psm_mvScore[tmp_psm]))
+            
+            sample_name = tmp_psm.split('.')[0]
+            sample_list.append(sample_name)
             tmp_pep = tmp_psm.split('.')[-1]
             if( not pep_count.has_key(tmp_pep) ):
-                pep_count[tmp_pep] = 0
-            pep_count[tmp_pep] += 1
+                pep_count[tmp_pep] = dict()
+            if( pep_count[tmp_pep].has_key(sample_name) ):
+                pep_count[tmp_pep][sample_name] = 0
+            pep_count[tmp_pep][sample_name] += 1
         else:
             if( not D_pep_count.has_key(tmp_pep) ):
                 D_pep_count[tmp_pep] = 0
             D_pep_count[tmp_pep] += 1
 f_log.close()
 
+sample_list = sorted(list(set(sample_list)))
+
 sys.stderr.write('Peptide FDR: %.3f\n'%( float(len(D_pep_count))/(len(pep_count)+len(D_pep_count)) ))
 f_count = open('%s.pep_count_FDR0005'%filename_base,'w')
+f_count.write('#PepSeq\tTotalCount\t%s\n'%('\t'.join(sample_list)))
 for tmp_pep in sorted(pep_count.keys(),key=pep_count.get):
-    f_count.write('%s\t%d\n'%(tmp_pep,pep_count[tmp_pep]))
+    out_list = []
+    total_count = 0
+    for tmp_sample in sample_list:
+        if( pep_count[tmp_pep].has_key(tmp_sample) ):
+            out_list.append( "%d"%pep_count[tmp_pep][tmp_sample] )
+            total_count += pep_count[tmp_pep][tmp_sample]
+        else:
+            out_list.append('0')
+
+    f_count.write('%s\t%d\t%s\n'%(tmp_pep,total_count,'\t'.join(out_list)))
 f_count.close()
