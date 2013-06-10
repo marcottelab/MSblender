6d496b7f2133311acc72a6ebdbec1e8a597cf609
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-06-10 14:19:22 -0500

    Change mzML to mzXML in MSGFDB scripts (MSGFDB is not supporting mzML)

--- search/longhorn-MSGFDB.sh
+++ search/longhorn-MSGFDB.sh
@@ -21,9 +21,9 @@ MSGFDB_JAR="$HOME/src.MS/MSGFDB/20120607/MSGFDB.jar"
 DBNAME=$(basename $DB)
 DBNAME=${DBNAME/.fa/}
 
-for MZML in $(ls ../mzML/*mzML)
+for MZXML in $(ls ../mzXML/*mzXML)
 do
-  OUT=$(basename $MZML)
-  OUT=${OUT/.mzML/}"."$DBNAME".MSGFDB_out"
-  time java -Xmx8000M -jar $MSGFDB_JAR -d $DB -s $MZML -t 30ppm -e 1 -nnet 0 -n 5 -o $OUT
+  OUT=$(basename $MZXML)
+  OUT=${OUT/.mzXML/}"."$DBNAME".MSGFDB_out"
+  time java -Xmx8000M -jar $MSGFDB_JAR -d $DB -s $MZXML -t 30ppm -e 1 -nnet 0 -n 5 -o $OUT
 done
