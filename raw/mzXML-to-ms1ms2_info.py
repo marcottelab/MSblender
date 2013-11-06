2c96443b07f284de435530f7bff389a310359762
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-11-06 11:37:34 -0600

    New scripts to inspect mzXML files

---
+++ raw/mzXML-to-ms1ms2_info.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import os 
+import sys
+
+filename_mzXML = sys.argv[1]
+
+scan_id = 0
+scan_info = dict()
+f_mzXML = open(filename_mzXML,'r')
+for line in f_mzXML:
+    line = line.strip()
+    if( line.startswith('<scan ') ):
+        scan_id = int(line.replace('"','').split('=')[1])
+        if( not scan_info.has_key(scan_id) ):
+            scan_info[scan_id] = {'ms2_count':0}
+    if( line.startswith('msLevel=') ):
+        scan_info[scan_id]['msLevel'] = int(line.replace('"','').split('=')[1])
+    if( line.startswith('retentionTime=') ):
+        scan_info[scan_id]['ret_time'] = float(line.replace('"','').split('=')[1].lstrip('PT').rstrip('S'))
+    if( line.startswith('totIonCurrent=') ):
+        scan_info[scan_id]['tot_ion'] = float(line.replace('"','').split('=')[1])
+      
+    if( line.startswith('<precursorMz') ):
+        for tmp in line.split()[1:]:
+            tmp = tmp.split('>')[0]
+            if( tmp.startswith('precursorScanNum') ):
+                tmp_precursor_id = int(tmp.replace('"','').split('=')[1])
+                scan_info[scan_id]['precursor_id'] = tmp_precursor_id
+                scan_info[tmp_precursor_id]['ms2_count'] += 1
+            if( tmp.startswith('precursorCharge') ):
+                scan_info[scan_id]['precursor_charge'] = int(tmp.replace('"','').split('=')[1])
+        scan_info[scan_id]['precursor_mz'] = float(line.split('>')[1].split('<')[0])
+f_mzXML.close()
+
+def get_param(args, keyword):
+    if( args.has_key(keyword) ):
+        return args[keyword]
+    return 0.0
+
+f_ms1 = open('%s.ms1_info'%filename_mzXML,'w')
+f_ms2 = open('%s.ms2_info'%filename_mzXML,'w')
+f_ms1.write('#ScanID\tRetTime\tCountMS2\tTotIon\n')
+f_ms2.write('#ScanID\tRetTime\tPrecursorID\tPrecursorCharge\tPrecursorMz\tTotIon\n')
+for tmp_scan_id in sorted(scan_info.keys()):
+    tmp = scan_info[tmp_scan_id]
+    if( tmp['msLevel'] == 1 ):
+        f_ms1.write('%05d\t%.4f\t%d\t%d\n'%(tmp_scan_id, tmp['ret_time'], tmp['ms2_count'], tmp['tot_ion']))
+    if( tmp['msLevel'] == 2 ):
+        tmp_ret_time = get_param(tmp, 'ret_time')
+        tmp_precursor_charge = get_param(tmp, 'precursor_charge')
+        tmp_precursor_id = get_param(tmp, 'precursor_id')
+        tmp_precursor_mz = get_param(tmp, 'precursor_mz')
+        tmp_tot_ion = get_param(tmp, 'tot_ion')
+        f_ms2.write('%05d\t%.4f\t%05d\t%d\t%.5f\t%.2f\n'%(tmp_scan_id, tmp_ret_time, tmp_precursor_id, tmp_precursor_charge,tmp_precursor_mz,tmp_tot_ion))
+f_ms1.close()
+f_ms2.close()
---
+++ raw/plot-ms1_info.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import os
+import sys
+import math
+
+filename_ms1 = sys.argv[1]
+filename_base = filename_ms1.replace('.ms1_info','').replace('.mzXML','').replace('.mzXML','')
+
+rt2ms2 = dict()
+rt2tot_ion = dict()
+
+f_ms1 = open(filename_ms1,'r')
+headers = f_ms1.readline().strip().split("\t")
+if( len(headers) != 4 ):
+    sys.stderr.write('Format error (not ms1_info file): %s\n'%filename_ms1)
+    sys.exit(1)
+
+for line in f_ms1:
+    tokens = line.strip().split("\t")
+    tmp_ret_time = float(tokens[1])
+    rt2ms2[tmp_ret_time] = int(tokens[2])
+    rt2tot_ion[tmp_ret_time] = int(tokens[3])
+f_ms1.close()
+
+ret_time_list = sorted(rt2ms2.keys())
+
+#import matplotlib
+#matplotlib.use('Agg')
+
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(10,8))
+
+ax1 = fig.add_subplot(2,1,1)
+ax1.plot(ret_time_list, [math.log10(rt2tot_ion[x]) for x in ret_time_list], 'k-', label='total ions')
+ax1.set_xlabel('Retention time (sec)')
+ax1.set_ylabel('Total Ion Intensity, log10')
+ax1.set_xlim(0,max(ret_time_list))
+ax1.set_title("%s (ms1=%d, ms2=%d)"%(filename_base,len(rt2ms2),sum(rt2ms2.values())))
+ax1.grid()
+
+ax2 = fig.add_subplot(2,1,2)
+ax2.plot(ret_time_list, [rt2ms2[x] for x in ret_time_list], 'k-', label='ms2 count')
+ax2.set_xlim(0,max(ret_time_list))
+ax2.set_ylim(0,max(rt2ms2.values())+1)
+ax2.set_xlabel('Retention time (sec)')
+ax2.set_ylabel('MS2 count')
+ax2.grid()
+
+filename_png = '%s.png'%filename_ms1
+sys.stderr.write('Write %s\n'%filename_png)
+plt.savefig(filename_png)
