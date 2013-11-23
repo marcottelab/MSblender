b850d78e75e2a7ef4f25b3a3006965d64f77dc01
Taejoon Kwon <taejoon.kwon@marcottelab.org>
2013-11-23 16:56:38 -0600

    Revise running scripts.

--- search/run-comet.sh
+++ search/run-comet.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-COMET="$HOME/src.MS/comet/2013010/comet.2013010.linux.exe"
+COMET="$HOME/git/MSblender/extern/comet.linux.exe"
+## Change this variable if you want to use your own params.
+PARAM="$HOME/git/MSblender/search/comet.params"
 
-for MZML in $(ls ../mzML/*mzML)
+## Path for the database, with decoy
+DB="/work/ups/db/UPS2013_combined.fa"
+
+## Extension of your raw file: mzXML, mzML, etc
+RAW_TYPE="mzXML"
+
+DBNAME=$(basename $DB)
+DBNAME=${DBNAME/.fasta/}
+DBNAME=${DBNAME/.fa/}
+
+for RAW in $(ls ../mzXML.5ul/*$RAW_TYPE)
 do
-  OUT=$(basename $MZML)
-  OUT=$PWD"/"${OUT/.mzML/}
-  time $COMET -N$OUT $MZML
+  OUT=$(basename $RAW)
+  OUT=$PWD"/"${OUT/.$RAW_TYPE/}"."$DBNAME".comet"
+  time $COMET -P$PARAM -N$OUT -D$DB $RAW
 done
