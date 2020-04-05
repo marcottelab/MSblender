4de80266097a8570ec2922b6ea231674a9eed705
Taejoon Kwon <tkwon@unist.ac.kr>
2020-04-05 02:55:14 +0000

    Generalize input/db for comet search

--- search/run-comet.sh
+++ search/run-comet.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 COMET="$HOME/git/MSblender/extern/comet.linux.exe"
+
 ## Change this variable if you want to use your own params.
 PARAM="$HOME/git/MSblender/search/comet.params"
 
 ## Path for the database, with decoy
-DB="/work/ups/db/UPS2013_combined.fa"
+## Make a symbolic link of your database as "COMBINED.fa"
+DB="COMBINED.fa"
 
 ## Extension of your raw file: mzXML, mzML, etc
 RAW_TYPE="mzXML"
@@ -13,7 +15,7 @@ DBNAME=$(basename $DB)
 DBNAME=${DBNAME/.fasta/}
 DBNAME=${DBNAME/.fa/}
 
-for RAW in $(ls ../mzXML.5ul/*$RAW_TYPE)
+for RAW in $(ls ../mzXML/*$RAW_TYPE)
 do
   OUT=$(basename $RAW)
   OUT=$PWD"/"${OUT/.$RAW_TYPE/}"."$DBNAME".comet"
